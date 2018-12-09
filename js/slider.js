f731118739c9b8894b1ccf96ab53e4a63b4f2a5b
Wired <desire0417@gmail.com>
2018-12-09 14:29:51 +0300

    slider основной

--- js/slider.js
+++ js/slider.js
@@ -32,14 +32,14 @@
   function goToSlide(n) {
     slides[currentSlide].className = 'slider-item';
     currentSlide = (n + slides.length) % slides.length;
-    slides[currentSlide].className = 'slider-item slider-item--active';
+    slides[currentSlide].className = 'slider-item slider-item-active';
 
-    find(slides, 'slider-item--active');
+    find(slides, 'slider-item-active');
 
     pagination.forEach(function (elem) {
-      elem.classList.remove('pagination-btn--active');
+      elem.classList.remove('pagination-btn-active');
     });
-    pagination[index].classList.add('pagination-btn--active');
+    pagination[index].classList.add('pagination-btn-active');
 
     if (slideInterval) {
       clearInterval(slideInterval);
