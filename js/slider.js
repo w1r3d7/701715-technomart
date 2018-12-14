17acd93823ec13e81477eef106ab9c556963020a
Wired <desire0417@gmail.com>
2018-12-14 12:35:27 +0300

    развлекуха с модальными окнами

--- js/slider.js
+++ js/slider.js
@@ -71,12 +71,12 @@ function active(elem) {
   }
   elem.classList.add('active-button');
 
-  var delivery = document.getElementById('del');
-  var deliveryItem = document.getElementById('delivery-item');
-  var guarantee = document.getElementById('gua');
-  var guaranteItem = document.getElementById('guarantee-item');
-  var credit = document.getElementById('cre');
-  var creditItem = document.getElementById('credit-item');
+  var delivery = document.querySelector('.delivery-btn');
+  var deliveryItem = document.querySelector('.delivery-item');
+  var guarantee = document.querySelector('.guarantee-btn');
+  var guaranteItem = document.querySelector('.guarantee-item');
+  var credit = document.querySelector('.credit-btn');
+  var creditItem = document.querySelector('.credit-item');
 
   if(delivery.classList.contains('active-button')) {
       deliveryItem.classList.add('service-active');
@@ -97,27 +97,124 @@ function active(elem) {
   }
 }
 
-var feedbackModal = document.getElementById('feedback-modal');
-var openFeedback = document.getElementById('open-feedback-modal');
+var feedbackModal = document.querySelector('.modal-login');
+var inputName = feedbackModal.querySelector('[name="name"]');
+var inputEmail = feedbackModal.querySelector('[name="email"]');
+var openFeedback = document.querySelector('.btn-contacts');
+var form = feedbackModal.querySelector('form');
 
 var closeModal = document.getElementsByClassName('modal-close');
 
-var mapModal = document.getElementById('map-modal');
-var openMap = document.getElementById('open-map-modal');
+var mapModal = document.querySelector('.modal-map');
+var openMap = document.querySelector('.map-link');
 
-openFeedback.onclick = function () {
+openFeedback.addEventListener("click", function (evt) {
+  evt.preventDefault();
   feedbackModal.classList.add('active-modal');
-}
+  inputName.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!inputName.value || !inputEmail.value ) {
+    evt.preventDefault();
+    feedbackModal.classList.remove('error-modal');
+    feedbackModal.classList.add("error-modal");
+  } else {
+    feedbackModal.classList.add('modal-ok');
+  }
+});
 
 closeModal[0].onclick = function () {
   feedbackModal.classList.remove('active-modal');
+  feedbackModal.classList.remove('error-modal')
+  feedbackModal.classList.remove('modal-ok');
 }
 
-openMap.onclick = function () {
-  openMap.removeAttribute('href');
+openMap.onclick = function (evt) {
+  evt.preventDefault();
   mapModal.classList.add('active-modal');
 }
 
 closeModal[1].onclick = function () {
   mapModal.classList.remove('active-modal');
+}
+
+var modalCart = document.querySelector('.modal-cart-add');
+var cartBook = document.querySelectorAll('.bookmark');
+var cartBuy = document.querySelectorAll('.buy');
+var cartCon = document.querySelector('.continue');
+var bookmarkBlock = document.querySelector('.header-link-bookmarks');
+var cartBlock = document.querySelector('.header-link-cart');
+var output = document.querySelectorAll('output');
+var bookmarksPlus = 0;
+var cartPlus = 0;
+
+cartBook[0].addEventListener("click", function(evt){
+  evt.preventDefault();
+  bookmarksPlus++;
+  output[0].innerHTML = bookmarksPlus;
+  bookmarkBlock.classList.add('not-empty');
+});
+
+cartBook[1].addEventListener("click", function(evt){
+  evt.preventDefault();
+  bookmarksPlus++;
+  output[0].innerHTML = bookmarksPlus;
+  bookmarkBlock.classList.add('not-empty');
+});
+
+cartBook[2].addEventListener("click", function(evt){
+  evt.preventDefault();
+  bookmarksPlus++;
+  output[0].innerHTML = bookmarksPlus;
+  bookmarkBlock.classList.add('not-empty');
+});
+
+cartBook[3].addEventListener("click", function(evt){
+  evt.preventDefault();
+  bookmarksPlus++;
+  output[0].innerHTML = bookmarksPlus;
+  bookmarkBlock.classList.add('not-empty');
+});
+
+cartBuy[0].addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalCart.classList.add('active-modal');
+  cartPlus++;
+  output[1].innerHTML = cartPlus;
+  cartBlock.classList.add('not-empty');
+});
+
+cartBuy[1].addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalCart.classList.add('active-modal');
+  cartPlus++;
+  output[1].innerHTML = cartPlus;
+  cartBlock.classList.add('not-empty');
+});
+
+cartBuy[2].addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalCart.classList.add('active-modal');
+  cartPlus++;
+  output[1].innerHTML = cartPlus;
+  cartBlock.classList.add('not-empty');
+});
+
+cartBuy[3].addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalCart.classList.add('active-modal');
+  cartPlus++;
+  output[1].innerHTML = cartPlus;
+  cartBlock.classList.add('not-empty');
+});
+
+cartCon.onclick = function (evt) {
+  evt.preventDefault();
+  modalCart.classList.remove('active-modal');
+}
+
+closeModal[2].onclick = function (evt) {
+  evt.preventDefault();
+  modalCart.classList.remove('active-modal');
 }
