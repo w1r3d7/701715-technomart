e822ad4bd4f4273ddf8ee5ef57c33a0fcc1d1ccf
Wired <desire0417@gmail.com>
2018-12-16 19:03:44 +0300

    Форматирование и исправление косяков

--- js/catalog.js
+++ js/catalog.js
@@ -43,4 +43,61 @@ for (var i = 0; i < cartBook.length; i++) {
       
 }
 
+'use strict';
+
+(function () {
+var modalCart = document.querySelector('.modal-cart-add');
+var cartBook = document.querySelectorAll('.bookmark');
+var cartBuy = document.querySelectorAll('.buy');
+var cartCon = document.querySelector('.continue');
+var bookmarkBlock = document.querySelector('.header-link-bookmarks');
+var cartBlock = document.querySelector('.header-link-cart');
+var output = document.querySelectorAll('output');
+var bookmarksPlus = 0;
+var cartPlus = 0;
+var closeModal = document.querySelector('.modal-close');
+
+
+cartCon.onclick = function (evt) {
+        evt.preventDefault();
+        modalCart.classList.remove('active-modal');
+}
+
+closeModal.onclick = function (evt) {
+        evt.preventDefault();
+        modalCart.classList.remove('active-modal');
+}
+
+for (var i = 0; i < cartBuy.length; i++) {
+        cartBuy[i].addEventListener("click", function(evt) {
+        evt.preventDefault();
+        modalCart.classList.add('active-modal');
+        cartPlus++;
+        output[1].innerHTML = cartPlus;
+        cartBlock.classList.add('not-empty');
+});
+}
+
+for (var i = 0; i < cartBook.length; i++) {
+    cartBook[i].addEventListener("click", function(evt){
+        evt.preventDefault();
+        bookmarksPlus++;
+        output[0].innerHTML = bookmarksPlus;
+        bookmarkBlock.classList.add('not-empty');
+      });
+      
+}
+
 })();
+
+window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+          evt.preventDefault();
+          if (modalCart.classList.contains("active-modal")) {
+            modalCart.classList.remove("active-modal");
+          }
+        }
+      })
+      
+})();
+
--- js/index.js
+++ js/index.js
@@ -1,69 +1,4 @@
 'use strict';
-/*
-(function () {
-
-  var slider = document.querySelector('.slider-list');
-  var slides = slider.querySelectorAll('.slider-item');
-  var next = document.querySelector('.slide-left');
-  var previous = document.querySelector('.slide-right');
-  var pagination = document.querySelectorAll('.pagination-btn');
-  var currentSlide = 0;
-  var INTERVAL = 5000;
-  var slideInterval = setInterval(nextSlide, INTERVAL);
-  var index;
-
-  function nextSlide() {
-    goToSlide(currentSlide + 1);
-  }
-
-  function previousSlide() {
-    goToSlide(currentSlide - 1);
-  }
-
-  function find(array, value) {
-    for (var i = 0; i < array.length; i++) {
-      if (array[i].classList.contains(value)) {
-        index = i;
-        return index;
-      }
-    }
-  }
-
-  function goToSlide(n) {
-    slides[currentSlide].className = 'slider-item';
-    currentSlide = (n + slides.length) % slides.length;
-    slides[currentSlide].className = 'slider-item slider-item-active';
-
-    find(slides, 'slider-item-active');
-
-    pagination.forEach(function (elem) {
-      elem.classList.remove('pagination-btn-active');
-    });
-    pagination[index].classList.add('pagination-btn-active');
-
-    if (slideInterval) {
-      clearInterval(slideInterval);
-    }
-    slideInterval = setInterval(nextSlide, INTERVAL);
-  }
-
-  next.onclick = function () {
-    nextSlide();
-  };
-  previous.onclick = function () {
-    previousSlide();
-  };
-
-  pagination.forEach(function (elem, i) {
-    elem.addEventListener('click', function () {
-      goToSlide(i);
-    });
-  });
-  
-  
-})();
-*/
-
 
 function active(elem) {
   var a = document.getElementsByTagName('button')
@@ -121,15 +56,12 @@ form.addEventListener("submit", function (evt) {
     feedbackModal.classList.remove('error-modal');
     void feedbackModal.offsetWidth;
     feedbackModal.classList.add("error-modal");
-  } else {
-    feedbackModal.classList.add('modal-ok');
   }
 });
 
 closeModal[0].onclick = function () {
   feedbackModal.classList.remove('active-modal');
   feedbackModal.classList.remove('error-modal')
-  feedbackModal.classList.remove('modal-ok');
 }
 
 openMap.onclick = function (evt) {
@@ -180,3 +112,32 @@ closeModal[2].onclick = function (evt) {
   evt.preventDefault();
   modalCart.classList.remove('active-modal');
 }
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (feedbackModal.classList.contains("active-modal")) {
+      feedbackModal.classList.remove("active-modal");
+      feedbackModal.classList.remove("error-modal");
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapModal.classList.contains("active-modal")) {
+      mapModal.classList.remove("active-modal");
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modalCart.classList.contains("active-modal")) {
+      modalCart.classList.remove("active-modal");
+    }
+  }
+});
